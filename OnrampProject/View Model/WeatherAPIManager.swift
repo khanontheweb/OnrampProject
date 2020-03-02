71a92387eb3df05732a2988f492b1a02c7b72932
Momo Khan <mkhanongm@gmail.com>
2020-03-02 17:00:42 -0500

    Perform API Call, parse JSON, use Codable WeatherModel to create WeatherViewModel

--- OnrampProject/View Model/WeatherAPIManager.swift
+++ OnrampProject/View Model/WeatherAPIManager.swift
@@ -20,6 +20,43 @@ struct WeatherAPIManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(url)&q=\(cityName)"
-        print(urlString)
+        performRequest(with: urlString)
+        
     }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let validData = data {
+                    if let weather = self.parseJSON(validData) {
+                        print(weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    
+    func parseJSON(_ weatherData: Data) -> WeatherViewModel? {
+        let decoder = JSONDecoder()
+        do {
+            let weatherModel = try decoder.decode(WeatherModel.self, from: weatherData)
+            let weatherViewModel = WeatherViewModel(conditionId: weatherModel.weather[0].id, cityName: weatherModel.name, temperature: weatherModel.main.temp)
+            return weatherViewModel
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+    
+    
+    
+    
 }
