6b7887aaa0e6f2ce0234f838a20fa92b71cb527e
Momo Khan <mkhanongm@gmail.com>
2020-03-02 13:55:14 -0500

    Create MVVM structure

---
+++ OnrampProject/Model/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  OnrampProject
+//
+//  Created by Momo Khan on 3/2/20.
+//
+
+import Foundation
---
+++ OnrampProject/View Model/WeatherViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherViewModel.swift
+//  OnrampProject
+//
+//  Created by Momo Khan on 3/2/20.
+//
+
+import Foundation
