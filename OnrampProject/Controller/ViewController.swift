f5f24cf14f4fbf18a71892542c1cdfaa29f0c36f
Momo Khan <mkhanongm@gmail.com>
2020-03-02 16:02:24 -0500

    UITextField prints user input

--- OnrampProject/Controller/ViewController.swift
+++ OnrampProject/Controller/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherConditionImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cityTextField.delegate = self
     }
 
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        cityTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        cityTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            //put an alert here later instead if time allows
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = cityTextField.text {
+            print(city)
+        }
+        
+        cityTextField.text = ""
+    }
+    
+    
 }
 
